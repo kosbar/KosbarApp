ec1e1e3530fc3a731d786e2add8eedffc12fb16b
Programm <kosbar28@gmail.com>
2019-06-21 13:20:24 +0500

    some fixes

--- app/src/main/java/com/example/kosbarapp/MainActivity.java
+++ app/src/main/java/com/example/kosbarapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kosbarapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -11,37 +12,41 @@ import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends AppCompatActivity {
+    public final String TAG = "kosbarApp:";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "HELLO ANDROID WORLD");
         CallbackManager callbackManager = CallbackManager.Factory.create();
 
 
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        LoginButton loginButton = findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
         // If using in a fragment
-        loginButton.setFragment(this);
+
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                // App code
+                Log.e(TAG, "ETO POBEDA");
             }
 
             @Override
             public void onCancel() {
-                // App code
+                Log.e(TAG, "ETO PORAZHENIE");
             }
 
             @Override
             public void onError(FacebookException exception) {
-                // App code
+                Log.e(TAG, "WTF?!?!?!?");
             }
         });
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
