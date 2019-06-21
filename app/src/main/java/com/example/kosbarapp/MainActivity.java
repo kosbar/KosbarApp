974c355228e8dac7b713b1e071600b670ef52dbd
Programm <kosbar28@gmail.com>
2019-06-21 19:39:10 +0500

    Working example

--- app/src/main/java/com/example/kosbarapp/MainActivity.java
+++ app/src/main/java/com/example/kosbarapp/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.kosbarapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
+
     public final String TAG = "kosbarApp:";
+    private LoginButton loginButton;
+    private TextView displayName, emailID;
+    private ImageView displayImage;
+    private CallbackManager callbackManager;
+
 
 
     @Override
@@ -21,24 +36,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        displayName = findViewById(R.id.display_name);
+        emailID = findViewById(R.id.email);
+        displayImage = findViewById(R.id.image_view);
+        loginButton = findViewById(R.id.login_button);
+
         Log.e(TAG, "HELLO ANDROID WORLD");
-        CallbackManager callbackManager = CallbackManager.Factory.create();
-
-
-        LoginButton loginButton = findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email");
+        callbackManager = CallbackManager.Factory.create();
+        //loginButton.setReadPermissions("email");
         // If using in a fragment
-
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Log.e(TAG, "ETO POBEDA");
+                AccessToken accessToken = loginResult.getAccessToken();
+                useLoginInformation(accessToken);
+                Log.e(TAG, "ETO POBEDA " + loginResult.getAccessToken().getDataAccessExpirationTime());
             }
 
             @Override
             public void onCancel() {
+
                 Log.e(TAG, "ETO PORAZHENIE");
             }
 
@@ -48,5 +67,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resulrCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resulrCode, data);
+        super.onActivityResult(requestCode, resulrCode, data);
+    }
+
+    private void useLoginInformation(AccessToken accessToken) {
+        /**
+         Creating the GraphRequest to fetch user details
+         1st Param - AccessToken
+         2nd Param - Callback (which will be invoked once the request is successful)
+         **/
+        GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+            //OnCompleted is invoked once the GraphRequest is successful
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String name = object.getString("name");
+                    String email = object.getString("email");
+                    String image = object.getJSONObject("picture").getJSONObject("data").getString("url");
+                    displayName.setText(name);
+                    emailID.setText(email);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        // We set parameters to the GraphRequest using a Bundle.
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email,picture.width(200)");
+        request.setParameters(parameters);
+        // Initiate the GraphRequest
+        request.executeAsync();
     }
 }
